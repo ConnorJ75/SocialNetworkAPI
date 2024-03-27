4b7a53c04dfc025a45a8f0d9f02190c7e8858942
Connor Johnson <connorjohnson@connors-air.mynetworksettings.com>
2024-03-27 18:29:43 -0400

    added final routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -12,6 +12,11 @@ const {
 
 router.route('/').get(getThoughts).post(createThought);
 
+router.route('/:thoughtId').get(getThoughtById).put(updateThoughtById).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,4 +12,10 @@ const {
 
 router.route('/').get(getUsers).post(createUser);
 
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
 module.exports = router;
