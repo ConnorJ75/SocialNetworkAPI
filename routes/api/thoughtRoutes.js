7286b65382176c7409036a56e7e2984ae0642cee
Connor Johnson <connorjohnson@connors-air.mynetworksettings.com>
2024-03-27 21:46:04 -0400

    commented out routes and controllers

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,13 +10,13 @@ const {
     deleteReaction
 } = require('../../controllers/thoughtController');
 
+//GET and POST routes
 router.route('/').get(getThoughts).post(createThought);
-
-router.route('/:thoughtId').get(getThoughtById).put(updateThoughtById).delete(deleteThought);
-
+//GET PUT and DELETE by id
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
+//create reaction for a thought route
 router.route('/:thoughtId/reactions').post(createReaction);
-
+//delete reaction for a thought route
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
-
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,13 +9,13 @@ const {
     addFriend,
     removeFriend
 } = require('../../controllers/userController');
-
+//GET and POST routes
 router.route('/').get(getUsers).post(createUser);
-
+//GET PUT and DELETE by id
 router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
-
+//route for adding friend for a user
 router.route('/:userId/friends/:friendId').post(addFriend);
-
+//route for removing(deleting) a friend for a user
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
